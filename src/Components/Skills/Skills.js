78252e1fb27a60440fce797701d2bb6811a6dc30
DogSniffers <DogSniffers@users.noreply.github.com>
2020-07-26 15:18:00 -0600

    Tech section filled out

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -18,9 +18,39 @@ function Skills(){
                 <div className='techHolder'>
                     <h1 className='skillsPageHead'>Tech</h1>
                     <div className='tech'>
-                        <ul>
-                            <li>Test</li>
-                        </ul>
+                        <div>
+                            <h2 className='techHeads'>Languages</h2>
+                                <ul>
+                                    <li>Java</li>
+                                    <li>HTML</li>
+                                    <li>JavaScript</li>
+                                    <li>CSS</li>
+                                </ul>
+                        </div>
+                        <div>
+                            <h2 className='techHeads'>Technologies</h2>
+                            <div>
+                                <h3 className='techHeads'>Front-End</h3>
+                                    <ul>
+                                        <li>ChartJS</li>
+                                        <li>Material.UI</li>
+                                        <li>React</li>
+                                        <li>SASS</li>
+                                </ul>
+                            </div>
+                            <div>
+                                <h3 className='techHeads'>Back-End</h3>
+                                    <ul>
+                                        <li>Axios</li>
+                                        <li>NodeJS</li>
+                                        <li>PostgreSQL</li>
+                                        <li>Postman</li>
+                                        <li>React Redux</li>
+                                        <li>Socket.io</li>
+                                        <li>SQL Tabs</li>
+                                    </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
