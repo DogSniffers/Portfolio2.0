5d7e243accdad3bb0bd308fd689dfb73d505aed5
DogSniffers <DogSniffers@users.noreply.github.com>
2020-07-30 21:18:01 -0600

    added project filler text, and aboutme component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import Landing from './Components/Landing/Landing'
 import Skills from './Components/Skills/Skills'
+import Projects from './Components/Projects/Projects'
 import './App.css';
 
 function App() {
   return (
     <>
     {/* <Landing/> */}
-    <Skills/>
+    {/* <Skills/> */}
+    <Projects/>
     </>
 
   );
---
+++ src/Components/Projects/Projects.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Projects.css'
+
+function Projects(){
+    return(
+        <div>
+            <div className='projectsHolder'>
+                <h1 className='projectsHead'>DevMountain Projects</h1>
+                <h3 className='projectInfoHead'> These are the projects that I worked on at my time attending DevMountain. The two major projects that I was assigned to work on was a Solo-Project and then a Group-Project</h3>
+                <div className='projectBox'>
+                    <h2 className='projectsSubHead'>The Gauntlet</h2>
+                    <p className='projectInfo'> The Gauntlet was my personal project that I worked on while attending DevMountain. The app itself is a turn-based  RPG game. It has features that allow for future additions and reworks/balance fixing, and allows the user to add their own 'monsters' into the game. The app was designed in a way to have a retro feel to it, similar to some of the originally developed games for the computer. The technologies that were used for this project were React, HTML, CSS, NodeJS, React-Redux, Axios, SASS, and ChartJS </p>
+                    <div className='projectImageBox'>
+                        <img></img>
+                    </div>
+                </div>
+                <div className='projectBox'>
+                    <h2 className='projectsSubHead'>Board-Game Hosting Site</h2>
+                    <p className='projectInfo'></p>
+                    <div className='projectImageBox'>
+                        <img></img>
+                    </div>
+                </div>
+                
+            </div>
+            <div className='projectsHolder'>
+                <h1 className='projectsHead'>Personal Projects</h1>
+                <h3 className='projectInfoHead'> These are the projects that I have worked on in my free time. </h3>
+                <div className='projectBox'>
+                    <h2 className='projectsSubHead'>Filler</h2>
+                    <p className='projectInfo'></p>
+                    <div className='projectImageBox'>
+                        <img></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Projects
